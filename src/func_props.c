8205b57ca12c0aba37999ac80c14b6273b2e994f
Daniel Fiser <danfis@danfis.cz>
2026-07-28 20:16:14 +0200

    func_props: Rename init-count to init-val-size in test output
    pddl_func_prop_t::init_count was replaced by the init_val[] array and
    its init_val_size counter. Follow the rename in the func_props test
    and refresh the golden baselines accordingly.

--- src/func_props.c
+++ src/func_props.c
@@ -8,7 +8,7 @@ TEST(func_props, pddl)
     for (int f = 0; f < fp.func_size; ++f){
         const pddl_func_prop_t *p = fp.prop + f;
         printf("(%s)\n", C.pddl.func.pred[f].name);
-        printf("  init-count: %d\n", p->init_count);
+        printf("  init-val-size: %d\n", p->init_val_size);
         printf("  init-all-nonneg: %d\n", p->init_all_nonneg);
         printf("  init-all-nonpos: %d\n", p->init_all_nonpos);
         printf("  init-all-int: %d\n", p->init_all_int);
